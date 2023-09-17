cf8266eadc3d552126bdc2cdb0f0a9c08544220a
Louis Guitton <louisguitton93@gmail.com>
2023-09-17 19:27:59 +0200

    fix mobile

--- app/(routes)/process/components/LogoCloud.tsx
+++ app/(routes)/process/components/LogoCloud.tsx
@@ -11,7 +11,7 @@ export default function LogoCloud() {
   return (
     <div className="bg-white py-24 sm:py-32">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
-        <div className="mx-auto grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-12 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 sm:gap-y-14 lg:mx-0 lg:max-w-none lg:grid-cols-6">
+        <div className="mx-auto grid max-w-lg grid-cols-3 items-center gap-x-8 gap-y-12 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 sm:gap-y-14 lg:mx-0 lg:max-w-none lg:grid-cols-6">
           <Link
             href="https://www.ifpenergiesnouvelles.com/"
             aria-label="IFP Energies Nouvelles Logo"
