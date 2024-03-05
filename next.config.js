e527592ac081664718fbb1ce83ecbf6eb09a636d
Louis Guitton <louisguitton93@gmail.com>
2024-03-05 12:02:12 +0100

    one more CSP try

--- next.config.js
+++ next.config.js
@@ -36,7 +36,7 @@ const ContentSecurityPolicy = `
     media-src 'none';
     connect-src *;
     font-src 'self' data:;
-    frame-src 'self' 'https://www.youtube.com/';
+    frame-src 'self' https://www.youtube.com/;
 `;
 
 const securityHeaders = [
