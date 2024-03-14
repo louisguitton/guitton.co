3e426c8c173046339cad739ad789d192753948b3
Louis Guitton <louisguitton93@gmail.com>
2024-03-14 16:57:04 +0100

    improve skills section

--- app/(routes)/resume/components/jsonresume/Skills.tsx
+++ app/(routes)/resume/components/jsonresume/Skills.tsx
@@ -7,12 +7,14 @@ const Skills: FC<{ skills: Skill[] }> = ({ skills }) => {
   if (skills.length) {
     return (
       <Section name="skills">
-        <div className="grid w-full grid-cols-1 sm:grid-cols-3 print:grid-cols-3">
+        <div className="grid w-full grid-cols-1 gap-1">
           {skills.map((skill, i) => {
             return (
-              <div key={i} className="col-span-1 pb-2 pr-2 overflow-hidden">
-                <h3 className="font-semibold">{skill.name}</h3>
-                <Keywords keywords={skill.keywords} itemIndex={i} />
+              <div key={i} className="overflow-hidden grid grid-cols-6">
+                <h3 className="font-semibold col-span-2">{skill.name}</h3>
+                <div className="col-span-4">
+                  <Keywords keywords={skill.keywords} itemIndex={i} />
+                </div>
               </div>
             );
           })}
